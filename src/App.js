6f2936341eb1f3b94203a76ff9993d52872f7d12
Daina <79340025+dazeste@users.noreply.github.com>
2023-02-16 16:58:51 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import axios from 'axios';
 
 
 function App() {
+  // const url="https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=312eff72f790313dee856b028b91820d";
+
+
+
+
   return (
-    <div className="App">
+    <div className="app">
+      <div className="container">
+        <div className="top"></div>
+        <div className="bottom"></div>
+
+      </div>
      
 
     </div>
