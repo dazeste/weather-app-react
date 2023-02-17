34fab24e9dd30a2cd0c4d5dd4f892e85225ae519
Daina <79340025+dazeste@users.noreply.github.com>
2023-02-17 00:05:00 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 
 function App() {
+ 
+  const url="https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=312eff72f790313dee856b028b91820d";
   // const url="https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=312eff72f790313dee856b028b91820d";
 
 
@@ -10,13 +12,21 @@ function App() {
 
   return (
     <div className="app">
+      <div className="search">
+        <input 
+        value=""
+        onChange =""
+        placeholder="Enter Location"
+        onKeyPress=""
+        type="text" />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
             <p>London</p>
           </div>
           <div className="temp">
-            <h1>10℃</h1>
+            <h1>10&#8451;</h1>
           </div>
           <div className="description">
             <p>Clouds</p>
@@ -24,13 +34,16 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>10℃</p>
+            <p className="bold">10&#8451;</p>
+            <p>Feels like</p>
           </div>
           <div className="humidity">
-          <p>75%</p>
+          <p className="bold">75%</p>
+          <p>Humidity</p>
           </div>
           <div className="wind">
-          <p>12 MPH</p>
+          <p className="bold">12 MPH</p>
+          <p>Wind</p>
           </div>
         </div>
 
